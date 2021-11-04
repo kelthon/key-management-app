0b57e302eaa3d03604d450e29eb03fe2ccd1c5a9
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2021-11-04 10:38:32 -0300

    Database initialization on index

--- flask/key_manager/routes/index.py
+++ flask/key_manager/routes/index.py
@@ -2,7 +2,15 @@
     Rota Principal
 '''
 from key_manager import app
+from key_manager.models import db
 
+@app.before_first_request
+def initialize():
+    try:
+        db.create_all()
+    except:
+        print("Erro ao inicializar sqlite")
+        
 @app.route('/')
 def index():
     return "Seja Bem-vindo!"
