247e9162e0d3b8c4783f223cb19738b356d1ab5b
muranoban <allanpereira.fenelon@gmail.com>
2021-11-19 13:49:53 -0300

    Atualização do código e insersão de novas funcionalidades

--- flask/key_manager/routes/index.py
+++ flask/key_manager/routes/index.py
@@ -1,5 +1,8 @@
 '''
     Rota Principal
+
+    add :  if session.get('autenticado',False)==False:
+       return (redirect(url_for('login')))
 '''
 from key_manager import app
 from key_manager.models import db
@@ -18,9 +21,14 @@ def initialize():
         
 @app.route('/')
 def index():
-    return "Seja Bem-vindo!"
+    return (render_template('index.html'))
+
+@app.route('/login')
+def login():
+    return (render_template('login.html'))
 
 @app.route('/<notfound>')
 def notFound(notfound):
-    return "Não encontrado!"
+    return (render_template('notfound.html'))
+
     
