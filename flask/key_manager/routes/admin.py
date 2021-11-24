7cfc68b31525eebf689950fe79dd790b86240904
Dante <mitcrushing@gmail.com>
2021-11-24 11:36:35 -0300

    atualização cadastro usuários

--- flask/key_manager/routes/admin.py
+++ flask/key_manager/routes/admin.py
@@ -19,8 +19,25 @@ def newKey():
     return "new key"
 
 @admin.route('/cadastrar_usuario', methods['GET', 'POST'])
-def cadastrar
-    if request.method == "POST"
+def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        password = request.form.get("password")
+        date = request.form.get("date")
+        level = request.form.get("level")
+        if name and username and email and phone and password and date and level:
+            r = User(name, username, email, phone, password, date, level)
+            db.session.add(r)
+            db.session.commit()
+            
+    return redirect(url_for("/"))
+    
+@admin.route('/cadastrar_emprestimo', methods['GET', 'POST'])
+def lend():
+    if request.method == "POST":
         user_id = request.form.get("user_name")
         loan_date = request.form.get("loan_date")
         return_date = request.form.get("return_date")
@@ -29,8 +46,8 @@ def cadastrar
         email = request.form.get("email")
         
         if user_id and loan_date and return_date and key_id and name and name and email:
-            r = Registry(user_id, loan_date, return_date, key_id, name, email)
-            db.session.add(r)
+            c = Registry(user_id, loan_date, return_date, key_id, name, email)
+            db.session.add(c)
             db.session.commit()
     
     return redirect(url_for("index"))
