d872d885fed114e76c03669aa99b134123cf607f
Dante <mitcrushing@gmail.com>
2021-11-24 11:48:13 -0300

    atualização função cadastrar

--- flask/key_manager/routes/admin.py
+++ flask/key_manager/routes/admin.py
@@ -2,6 +2,7 @@
     Rota dos admnistradores
 '''
 from key_manager import app
+from key_manager.models import db
 from flask import (
     Blueprint, render_template,
     request, url_for, redirect, 
@@ -16,5 +17,22 @@ def admHome():
 @admin.route("/new/key", methods=["GET", "POST"])
 def newKey():
     return "new key"
+
+@admin.route('/cadastrar_usuario', methods['GET', 'POST'])
+def cadastrar
+    if request.method == "POST"
+        user_id = request.form.get("user_name")
+        loan_date = request.form.get("loan_date")
+        return_date = request.form.get("return_date")
+        key_id = request.form.get("key_id")
+        name = request.form.get("name")
+        email = request.form.get("email")
+        
+        if user_id and loan_date and return_date and key_id and name and name and email:
+            r = Registry(user_id, loan_date, return_date, key_id, name, email)
+            db.session.add(r)
+            db.session.commit()
+    
+    return redirect(url_for("index"))
 
 app.register_blueprint(admin)
