b4454574dd67e92337bb8a334f5425c79c0ca795
Dante <mitcrushing@gmail.com>
2021-11-24 12:22:38 -0300

    Criação da pagina cadastro, atualização nas rotas

--- flask/key_manager/routes/index.py
+++ flask/key_manager/routes/index.py
@@ -31,4 +31,19 @@ def login():
 def notFound(notfound):
     return (render_template('notfound.html'))
 
+@app.route('/formulario', methods['GET', 'POST'])
+def cadastrar
+    if request.method == "POST"
+        user_id = request.form.get("user_name")
+        loan_date = request.form.get("loan_date")
+        return_date = request.form.get("return_date")
+        key_id = request.form.get("key_id")
+        name = request.form.get("name")
+        email = request.form.get("email")
+        
+        if user_id and loan_date and return_date and key_id and name and name and email:
+            r = Registry(user_id, loan_date, return_date, key_id, name, email)
+            db.session.add(r)
+            db.session.commit()
     
+    return redirect(url_for("index"))
