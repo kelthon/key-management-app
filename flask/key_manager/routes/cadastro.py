be7c01ba7eb03e930c030abd12f37e402de11c1f
isacpxc <isacpxc@gmail.com>
2021-12-13 12:11:08 -0300

    otimização bugs - correção de rotas - criação rota cadastro de notícias

--- flask/key_manager/routes/cadastro.py
+++ flask/key_manager/routes/cadastro.py
@@ -3,11 +3,13 @@ from key_manager.models import db
 from key_manager.models.category import Category
 from key_manager.models.key import Key
 from key_manager.models.user import User
+from key_manager.models.news import News
 from key_manager.models.registry import Registry
 from key_manager.forms.formUser import FormUser
 from key_manager.forms.formReg import RegForm
 from key_manager.forms.formKey import KeyForm
 from key_manager.forms.formCat import CatForm
+from key_manager.forms.formNews import NewsForm
 from flask import (
     Blueprint, render_template,
     request, url_for, redirect, 
@@ -20,76 +22,101 @@ cadastro = Blueprint("cadastro", __name__, url_prefix="/new")
 @cadastro.route("/category", methods=["GET", "POST"])
 def newCategory():
     catForm = CatForm()
-    if session.get("user_auth", False) and session["user_auth"] == True:
+    if session.get("user_auth", False) and session["user_permission"] != "normal":
         if request.method == "POST":
-            name = request.form["name"]
-            slug = request.form["slug"]
+            name = request.form.get("name")
+            slug = request.form.get("slug").lower()
             
             if catForm.validate_on_submit():
                 newCat = Category(name=name, slug=slug)
                 db.session.add(newCat)
                 db.session.commit()
                 return redirect(url_for("admin.admHome"))
-        return render_template("forms/cadastrar_categoria.html", form=catForm, action=url_for("cadastro.newCategory"), hidden_footer=True)
+        return render_template("forms/cadastrar_categoria.html", form=catForm, action=url_for("cadastro.newCategory"), title="Cadastrar Categoria", hidden_footer=True)
     return redirect(url_for('index'))
 
 @cadastro.route("/key", methods=["GET", "POST"])
 def newKey():
-    if session.get("user_auth", False) and session["user_auth"] == True:
+    if session.get("user_auth", False) and session["user_permission"] != "normal":
         keyForm = KeyForm()
         keyForm.key_category_id.choices = [(cat.id, cat.name) for cat in Category.query.order_by("name")]
         if request.method == "POST":
-            name = request.form["name"]
-            slug = request.form["slug"]
-
-            key_category_id = int(request.form["key_category_id"])
+            name = request.form.get("name")
+            slug = request.form.get("slug").lower()
+            key_avaliable = request.form.get("key_avaliable", False) == 'True'
+            key_category_id = int(request.form.get("key_category_id"))
             if keyForm.validate_on_submit():
-                newKey = Key(name=name, slug=slug, key_category_id=key_category_id)
+                newKey = Key(name=name, slug=slug, key_category_id=key_category_id, key_avaliable=key_avaliable)
                 db.session.add(newKey)
                 db.session.commit()
                 return redirect(url_for("admin.admHome"))
-        return render_template("forms/cadastrar_chave.html", form=keyForm, action=url_for('cadastro.newKey'), hidden_footer=True)
+        return render_template("forms/cadastrar_chave.html", form=keyForm, action=url_for('cadastro.newKey'), title="Cadastrar Chave", hidden_footer=True)
     return redirect(url_for('index'))
 @cadastro.route("/user", methods=["GET", "POST"])
 def newUser():
     userform = FormUser()
     if request.method == "POST":
-        name = request.form["name"]
-        username = request.form["username"]
-        email = request.form["email"]
-        phone = request.form["phone"]
-        password = request.form["password"]
+        name = request.form.get("name")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        password = request.form.get("password")
 
         hash_passsword = hashlib.md5(password.encode("utf8")).hexdigest()
 
         if userform.validate_on_submit():
-            try:
-                newuser = User(name=name, username=username, email=email, phone=phone, password=hash_passsword)
+            newuser = User(name=name, username=username, email=email, phone=phone, password=hash_passsword)
+            if newuser is None:
                 db.session.add(newuser)
                 db.session.commit()
-            except:
-                # já existe
-                pass
+                try:
+                    session['user_id'] = newuser.id
+                    session['user_username'] = newuser.username
+                    session['user_auth'] = True
+                    session['user_permission'] = newuser.usertype
+                    flash("Usuário cadastrado com Sucesso", "success_msg")
+                    return redirect(url_for('index'))
+                except:
+                    flash("Falha de autenticação da Sessão", "error_msg")
+                    return redirect(url_for('login'))
+            else:
+                flash("Usuário já existe", "error_msg")
             return redirect(url_for("view.viewUser",user_username=newuser.username))
-    return render_template("forms/cadastrar_usuario.html", form=userform, action=url_for('cadastro.newUser'))
+    return render_template("forms/cadastrar_usuario.html", form=userform, action=url_for('cadastro.newUser'), hidden_footer=True, title="Cadastrar Usuário")
 @cadastro.route('/registry', methods=['GET', 'POST'])
 def newRegistry():
-    if session.get("user_auth", False) and session["user_auth"] == True:    
+    if session.get("user_auth", False) and session["user_permission"] != "normal":   
         regForm = RegForm()
         regForm.user_id.choices = [(user.id, user.username) for user in User.query.order_by("username")]
         regForm.key_id.choices = [(key.id, key.name) for key in Key.query.order_by("name")]
 
         if request.method == "POST":
-            user_id = int(request.form["user_id"])
-            key_id = int(request.form["key_id"])
-            name = request.form["holder_name"]
-            email = request.form["holder_email"]
+            user_id = int(request.form.get("user_id"))
+            key_id = int(request.form.get("key_id"))
+            name = request.form.get("holder_name")
+            email = request.form.get("holder_email")
             
             if regForm.validate_on_submit():
                 newRegistry = Registry(user_id=user_id, key_id=key_id, holder_name=name, holder_email=email)
                 db.session.add(newRegistry)
                 db.session.commit()
                 return redirect(url_for("view.viewReg", reg_id=newRegistry.id))
-        return render_template("forms/cadastrar_emprestimo.html", form=regForm, action=url_for('cadastro.newRegistry'), hidden_footer=True)
+        return render_template("forms/cadastrar_emprestimo.html", form=regForm, action=url_for('cadastro.newRegistry'), title="Cadastrar Empréstimo", hidden_footer=True)
+    return redirect(url_for('index'))
+
+@cadastro.route('/news', methods=['GET', 'POST'])
+def newNews():
+    if session.get("user_auth", False) and session["user_permission"] != "normal":
+        newsForm = NewsForm()
+        if request.method == 'POST':
+            if newsForm.validate_on_submit:
+                news = News(title=request.form.get("title"), content=request.form.get("content"))
+                db.session.add(news)
+                db.session.commit()
+                flash("Noticia cadastrada com sucesso", "success_msg")
+            else:
+                flash("Validação falhou", "error_msg")
+        else:
+            return render_template("forms/cadastrar_noticias.html", form=newsForm, action=url_for('cadastro.newNews'), title="Cadastrar Notícia", hidden_footer=True)
     return redirect(url_for('index'))
 app.register_blueprint(cadastro)
