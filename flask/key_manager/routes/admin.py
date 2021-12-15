fb1b6c7824f99ce63ea8857d7a669cbcad45d1a2
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2021-12-14 21:12:21 -0300

    Implementação de nocas funcionalidades

--- flask/key_manager/routes/admin.py
+++ flask/key_manager/routes/admin.py
@@ -3,15 +3,17 @@
 '''
 from key_manager import app
 from key_manager.models import db
-from key_manager.models.category import Category
 from key_manager.models.key import Key
 from key_manager.models.user import User
+from key_manager.models.category import Category
 from key_manager.models.registry import Registry
+from key_manager.forms.formManager import ManagerForm
 from flask import (
     Blueprint, render_template,
     request, url_for, redirect, 
     flash, session
 )
+from datetime import datetime
 
 admin = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -19,8 +21,37 @@ admin = Blueprint("admin", __name__, url_prefix="/admin")
 def admHome():
     return render_template("admin/indexAdmin.html", hidden_footer=True, title="Painel Administrativo")
 
-@admin.route("/manager")
+@admin.route("/manager", methods=['GET', 'POST'])
 def manager_user_permissions():
+    if session.get("user_auth", False) and session["user_permission"] != "normal":
+        users = User.query.filter_by(usertype='normal').all()
+        managerForm = ManagerForm()
+        if request.method == 'POST':
+            user_username = request.form.get("username")
+            user = User.query.filter_by(username=user_username)
+            if user is None:
+                flash("Usuário não encontrado", "error_msg")
+                return redirect(url_for('admin.admHome'))
+            user.usertype = 'admin'
+            db.session.commit()
+            flash("Usuário promovido com sucesso", "success_msg")
+            return redirect(url_for('view.viewUser', user_username=user.username))
+        return render_template("admin/gerenciarAdms.html", form= managerForm, action=url_for('admin.manager_user_permissions'), users=users, title="Gerenciar Permissões", hidden_footer=True)
     return redirect(url_for('index'))
 
+@admin.route('/return/<reg_id>')
+def returnKey(reg_id):
+    if session.get("user_auth", False) and session["user_permission"] != "normal":
+        registry = Registry.query.filter_by(id=reg_id).first()
+        if registry is None:
+            flash("Registro não encontrado", "error_msg")
+            return redirect(url_for('admin.admHome'))
+        key = Key.query.filter_by(id=registry.key_id).first()
+        key.key_avaliable = True
+        registry.key_return_date = datetime.utcnow()
+        db.session.commit()
+        flash("Registro atualizado com sucesso", "success_msg")
+        return redirect(url_for('view.viewReg', reg_id=registry.id))
+    return redirect(url_for('index'))
+    
 app.register_blueprint(admin)
