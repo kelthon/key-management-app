2e42a74f3e676fe491068d26fffc6cc21163ca1d
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2021-12-14 21:13:29 -0300

    Correção bug de tamanho minimo do nomes

--- flask/key_manager/forms/formReg.py
+++ flask/key_manager/forms/formReg.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired, Length
 class RegForm(FlaskForm):
     user_id = SelectField("user_id", coerce=int, validators=[DataRequired()])
     key_id = SelectField("key_id",  validators=[DataRequired()])
-    holder_name = StringField("holder_name",  validators=[Length(min=9, max=100)])
+    holder_name = StringField("holder_name",  validators=[Length(min=4, max=100)])
     holder_email = EmailField("holder_email", validators=[DataRequired(), Length(min=10, max=100)])
     send = SubmitField("Criar Registro")
