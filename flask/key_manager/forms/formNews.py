59f63ba21a0083af4c63b12aca2ab60b1d050dcf
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2021-12-29 18:28:54 -0300

    Atualizações nos forms

--- flask/key_manager/forms/formNews.py
+++ flask/key_manager/forms/formNews.py
@@ -3,6 +3,6 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class NewsForm(FlaskForm):
-    title = StringField("title", validators=[DataRequired(), Length(min=5, max=150)])
+    title = StringField("title", validators=[DataRequired(), Length(min=20, max=150)])
     content = TextAreaField("content", validators=[DataRequired(), Length(min=5, max=2000)])
     send = SubmitField("Confirmar")
